97306096cba953dcfd2958b2dfc0f97f3b796a5d
Mishieck <mishieckmwale@gmail.com>
2024-01-06 11:13:57 +0200

    feat(record-to-array-converter): rename record-to-array to record-converter

--- lib/record-to-array/record-to-array.ts
+++
@@ -1,6 +0,0 @@
-import type { DatabaseRecord, Index } from '../types/database';
-
-export const convertRecordToArray =
-  <Rec extends DatabaseRecord>(fields: Index<string>) =>
-  (record: Rec) =>
-    Object.entries(fields).map(([key]) => record[key as keyof Rec] ?? null);
