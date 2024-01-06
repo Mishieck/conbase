6e315390e1fc65b9aaaf00431882a1eddfa60ea6
Mishieck <mishieckmwale@gmail.com>
2024-01-06 13:32:29 +0200

    test(select/many): add tests

--- lib/select/select.test.ts
+++ lib/select/select.test.ts
@@ -1,13 +1,16 @@
 import { describe, it, expect } from 'bun:test';
 import type { Collection, Index } from '../types/database';
 import { selectOne } from './one';
+import { selectMany } from './many';
 
 type User = { id: string; name: string };
 
 const fields: Index<keyof User> = { id: 0, name: 1 };
 const userRecord = { id: '1', name: 'Name' };
 const createUser = (): User => ({ ...userRecord });
-const userArray: Array<User[keyof User]> = [userRecord.id, userRecord.name];
+const createArray = (record: User) =>
+  [record.id, record.name] as Array<User[keyof User]>;
+const userArray = createArray(userRecord);
 
 const createCollection = (useProxy = true): Collection<User> => ({
   name: 'Users',
@@ -35,5 +38,28 @@ describe('selectOne', () => {
     const collection = createCollection(false);
     const { data: record } = selectOne(collection)(userRecord.id);
     expect(record).toEqual(expectedRecord);
+  });
+});
+
+describe('selectMany', () => {
+  it('should select many records', () => {
+    const records = [createUser(), { ...createUser(), id: '2' }];
+
+    const collection = {
+      ...createCollection(),
+      records: records.map(createArray)
+    };
+
+    const { data } = selectMany<User>(collection)('1', '2');
+
+    records.forEach((expected, i) => {
+      const actual = data?.[i];
+
+      for (const key in fields) {
+        expect(expected[key as keyof User]).toBe(
+          actual?.[key as keyof User] ?? ''
+        );
+      }
+    });
   });
 });
