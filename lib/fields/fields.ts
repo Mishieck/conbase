c405660f825df4ab0db579bdf8fb5117930dce04
Mishieck <mishieckmwale@gmail.com>
2024-02-12 19:28:10 +0200

    feat(fields): move code to colleciton-handler

--- lib/fields/fields.ts
+++
@@ -1,7 +0,0 @@
-import type { Index } from '../types/database';
-
-export const createFields = (record: Record<string, unknown>): Index<string> =>
-  Object.entries(record).reduce(
-    (fields, [key], i) => ({ ...fields, [key]: i }),
-    {}
-  );
